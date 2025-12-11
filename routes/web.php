85963203049bc9371a38f680b805ecd16cf9880f
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-12-11 09:13:30 +0900

    Fix HTTP method for video storage endpoint from GET to POST

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 Route::prefix('hv')->group(function () {
     Route::get('/videos', [VideoController::class, 'index'])->name('videos.index');
     Route::get('/videos/v2', [VideoController::class, 'indexV2'])->name('videos.indexV2');
-    Route::get('/api/videos/store', [VideoController::class, 'store']);
+    Route::post('/api/videos/store', [VideoController::class, 'store']);
     Route::get('/api/videos/max-timestamp', [VideoController::class, 'fetchMaxTimestamp']);
 });
 
