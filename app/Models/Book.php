3a041fc1c4e1f2499cebedc34867f8877527cf91
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-12-26 20:10:52 +0900

    Add pages relationship to Book model for ordered retrieval of BookPages

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function pages()
+    {
+        // ページ番号順に並んだ状態で取得できるようにする
+        return $this->hasMany(BookPage::class)->orderBy('page_number');
+    }
 }
